a432564805d5b6b52bb13ec87897cbf98f87976b
Temidayo Kehinde <temidayo.kehinde.10@gmail.com>
2023-07-13 16:23:02 +0100

    worked on responsiveness

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -24,43 +24,46 @@ const Home = () => {
   return (
     <div className="bg-blue-centric w-full m-0 text-eggshell h-screen flex flex-col justify-between">
       <Header />
-      <div className="text-eggshell pt-28 w-full flex flex-col gap-8 justify-between self-center">
-        <div className="flex flex-col w-full items-center self-center justify-between gap-4">
-          <img src={img} className="w-28 h-28 rounded-full  animate-wiggle " />
-          <div className="flex flex-col text-center justify-center ">
-            <p className="large:text-2xl text-xl flex-col w-full text-center">
-              <span className=" text-coral text-2xl large:text-3xl">
-                {texts[currentTextIndex]}{" "}
+      <div className="text-eggshell pt-28 w-5/6 ">
+        <div className="m-auto ">
+          <img src={img} className="w-2/6 h-96 float-right rounded-full  animate-wiggle " />
+          <div className="flex flex-col gap-4 justify-center">
+            <div className="flex flex-col justify-center ">
+              <p className="large:text-2xl text-xl flex-col w-full text-right">
+                <span className=" text-coral text-2xl large:text-3xl">
+                  {texts[currentTextIndex]}{" "}
+                </span>
+                <br className="large:hidden" />
+                <span>My name is </span>
+                <span className="text-coral large:text-5xl text-3xl">
+                  Temidayo Kehinde
+                </span>
+              </p>
+              <span className="large:text-xl text-sm large:text-right text-left">
+                Front-End Web Developer{" "}
+                <img src={tag} className="inline large:w-10 pl-2 w-8" />
               </span>
-              <br className="large:hidden" />
-              <span>My name is </span>
-              <span className="text-coral large:text-5xl text-3xl">
-                Temidayo Kehinde
-              </span>
-            </p>
-            <span className="large:text-xl text-sm large:text-right text-left">
-              Front-End Web Developer{" "}
-              <img src={tag} className="inline large:w-10 pl-2 w-8" />
-            </span>
-          </div>
-          <div className="tracking-wide capitalize text-center w-5/6 large:w-3/6 large:text-lg">
-            <p className="pb-4">
-              I am <span className="">passionate</span> about bridging the gap
-              between businesses and their customers by bringing{" "}
-              <span className=" "> user-friendly designs</span> to life using{" "}
-              <span className=""> codes.</span>
-            </p>
-            <p>
-              <span className="">
-                Eating. <span className="text-eggshell">Coding. </span> Singing.
-              </span>
-              <br />
-              ...These three things make me happy.
-            </p>
+            </div>
+            <div className="tracking-wide capitalize text-right w-5/6 large:text-lg flex flex-col gap-4 self-end">
+              <p className="text-right">
+                I am <span className="">passionate</span> about bridging the gap
+                between businesses and their customers by bringing{" "}
+                <span className=" "> user-friendly designs</span> to life using{" "}
+                <span className=""> codes.</span>
+              </p>
+              <p>
+                <span className="">
+                  Eating. <span className="text-eggshell">Coding. </span>{" "}
+                  Singing.
+                </span>
+                <br />
+                ...These three things make me happy.
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
